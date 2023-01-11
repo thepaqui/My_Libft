889c80608fe735e96809445510e2ad684be68d45
thepaqui <117783405+thepaqui@users.noreply.github.com>
2023-01-11 14:04:28 +0100

    Update ft_memset.c

--- memory/ft_memset.c
+++ memory/ft_memset.c
@@ -16,12 +16,9 @@ void	*ft_memset(void *b, int c, size_t len)
 	size_t			i;
 	unsigned char	*str;
 
-	i = 0;
 	str = b;
-	while (i < len)
-	{
+	i = -1;
+	while (++i < len)
 		*str++ = (unsigned char)c;
-		i++;
-	}
 	return (b);
 }
