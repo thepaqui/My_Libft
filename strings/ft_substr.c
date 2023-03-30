bbbf24a14f0fc78f8549b4840df7cc52f894a6be
thepaqui <117783405+thepaqui@users.noreply.github.com>
2023-03-30 20:50:45 +0200

    Update ft_substr.c

--- strings/ft_substr.c
+++ strings/ft_substr.c
@@ -19,16 +19,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (start >= ft_strlen(s))
 	{
-		sub = (char *)malloc(sizeof(*sub) * 1);
+		sub = ft_calloc(1, sizeof(*sub));
 		if (!sub)
 			return (NULL);
-		sub[0] = '\0';
 		return (sub);
 	}
 	if (len <= ft_strlen(s))
-		sub = (char *)malloc(sizeof(*sub) * (len + 1));
+		sub = ft_calloc(len + 1, sizeof(*sub));
 	else
-		sub = (char *)malloc(sizeof(*sub) * (ft_strlen(s) + 1));
+		sub = ft_calloc(ft_strlen(s) + 1, sizeof(*sub));
 	if (!sub)
 		return (NULL);
 	ft_strlcpy(sub, s + start, len + 1);
