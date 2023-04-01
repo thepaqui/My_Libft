f3951ddac11cbff7fcaece377f4093d0e4781e0a
thepaqui <117783405+thepaqui@users.noreply.github.com>
2023-04-01 22:23:00 +0200

    Create ft_strndup.c

---
+++ strings/ft_strndup.c
@@ -0,0 +1,16 @@
+char	*ft_strndup(const char *str, int n)
+{
+	char	*copy;
+	int		i;
+
+	if (!str)
+		return (NULL);
+	copy = ft_calloc(n + 1, sizeof(char));
+	if (!copy)
+		return (NULL);
+	i = -1;
+	while (str[++i] && i < n)
+		copy[i] = str[i];
+	copy[i] = '\0';
+	return (copy);
+}
