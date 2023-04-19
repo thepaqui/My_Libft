4f44b889d70821184bee2fb1ab7b5450c3588d55
thepaqui <117783405+thepaqui@users.noreply.github.com>
2023-04-19 15:35:34 +0200

    Create ft_split_with_empty.c

---
+++ strings/ft_split_with_empty.c
@@ -0,0 +1,50 @@
+#include "libft.h"
+
+static int	ft_strcount_with_empty(char const *str, char *set)
+{
+	int	i;
+	int	str_len;
+	int	str_count;
+
+	str_count = 0;
+	str_len = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isinset((int)str[i], set))
+			str_count++;
+		else
+			str_len++;
+		i++;
+	}
+	if (str_len > 0)
+		str_count++;
+	return (str_count);
+}
+
+char	**ft_split_with_empty(char const *s, char *set)
+{
+	char	**tab;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (NULL);
+	tab = ft_calloc((ft_strcount_with_empty(s, set) + 1), sizeof(char *));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < ft_strcount_with_empty(s, set) && s[j])
+	{
+		tab[i] = ft_calloc((ft_strclen(&s[j], set) + 1), sizeof(char));
+		if (!tab[i])
+			return (ft_free_tab(tab, i));
+		ft_strlcpy(tab[i++], &s[j], ft_strclen(&s[j], set) + 1);
+		while (s[j] && !ft_isinset((int)s[j], set))
+			j++;
+		j++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
