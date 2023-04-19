4537f926cd71bd7896296cf6cd121e3ac7912fa3
thepaqui <117783405+thepaqui@users.noreply.github.com>
2023-04-19 15:19:30 +0200

    Create replace_all_x_with_y.c

---
+++ strings/replace_all_x_with_y.c
@@ -0,0 +1,11 @@
+void    replace_all_x_with_y(char *nb, char x, char y)
+{
+    int i;
+
+    if (!nb)
+        return ;
+    i = -1;
+    while (nb[++i])
+        if (nb[i] == x)
+            nb[i] = y;
+}
