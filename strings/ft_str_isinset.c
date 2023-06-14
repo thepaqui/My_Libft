6fefb6234bd0d5cbf3ebfcc6410ab430c6a3260e
thepaqui <theopaquier2@gmail.com>
2023-06-14 19:35:02 +0200

    Dumb bitch I am

--- strings/ft_str_isinset.c
+++ strings/ft_str_isinset.c
@@ -6,7 +6,7 @@
 /*   By: thepaqui <thepaqui@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 18:16:20 by thepaqui          #+#    #+#             */
-/*   Updated: 2023/06/11 18:23:18 by thepaqui         ###   ########.fr       */
+/*   Updated: 2023/06/14 19:34:50 by thepaqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	ft_str_isinset(char *str, char *set)
 		return (0);
 	if (!set)
 		return (1);
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i])
 		if (!ft_isinset(str[i], set))
 			return (0);
 	return (1);
